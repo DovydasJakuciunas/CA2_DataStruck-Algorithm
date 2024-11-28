9c1a7374e36d1fdb0aacd976cfb3e915a72ea4da
DovydasJakuciunas <123966236+DovydasJakuciunas@users.noreply.github.com>
2024-11-28 12:05:03 +0000

    🚧 Re-Working Wrapping 🐛
    Changing it from map to be integrated to binary tree. More optimisation.

--- CA2-SearchTree/TreeMap.h
+++ CA2-SearchTree/TreeMap.h
@@ -1,85 +1,99 @@
-#include <map>  // For key-value mapping
-#include <vector>    
-
-#include "../CA2-SearchTree/BinaryTree.h" 
-
-template <typename K, typename V>
-class TreeMap {
-public:
-	BSTNode<K>* root;
-	map<K, V> keyValueMap;
-	int numElements;
-
-	TreeMap() : root(nullptr), numElements(0) {}
-	~TreeMap() { clear(); }
-
-    void put(K& key, V& value);
-	V get(K& key);
-    int size();
-	void clear();
-	bool containsKey(K& key);
-
-
-};
-
-
-template <class K, class V>
-void TreeMap<K, V>::put(K& key, V& value)
-{
-	if (root == nullptr)
-	{
-		root = new BSTNode<K>(key);
-		keyValueMap[key] = value;
-		numElements++;
-		
-	}
-	else
-	{
-		root->add(key);
-		keyValueMap[key] = value;
-		numElements++;
-	}
-	
-}
-
-template <class K, class V>
-V TreeMap<K, V>::get(K& key)
-{
-	auto it = keyValueMap.find(key);
-	if (it != keyValueMap.end())
-	{
-		return it->second;
-	}
-	else
-	{
-		return V(); 
-	}
-}
-
-template <class K, class V>
-int TreeMap<K, V>::size()
-{
-	return numElements;
-}
-
-template<typename K, typename V>
-inline void TreeMap<K, V>::clear()
-{
-
-	if (root != nullptr)
-	{
-		delete root;
-		root = nullptr;
-		numElements=0;
-	}
-	keyValueMap.clear();
-	
-	
-}
-
-template<typename K, typename V>
-inline bool TreeMap<K, V>::containsKey(K& key)
-{
-	return keyValueMap.find(key) != keyValueMap.end();
-}
-
+//#include "../CA2-SearchTree/BinaryTree.h" 
+//
+//template <typename K, typename V>
+//class TreeMap {
+//public:
+//	BSTNode<K>* root;
+//	BinaryTree<K, V> keyValueMap;
+//	int numElements;
+//
+//	TreeMap() : root(nullptr), numElements(0) {}
+//	~TreeMap() { clear(); }
+//
+//	void clear();
+//	bool containsKey(K& key);
+//	V get(K& key);
+//	BinaryTree<K> keySet();
+//    void put(K& key, V& value);
+//    int size();
+//	
+//	
+//
+//
+//};
+//
+//template<typename K, typename V>
+//inline void TreeMap<K, V>::clear()
+//{
+//
+//	if (root != nullptr)
+//	{
+//		delete root;
+//		root = nullptr;
+//		numElements = 0;
+//	}
+//	keyValueMap.clear();
+//
+//
+//}
+//
+//template<typename K, typename V>
+//inline bool TreeMap<K, V>::containsKey(K& key)
+//{
+//	return keyValueMap.find(key) != keyValueMap.end();
+//}
+//
+//template <class K, class V>
+//V TreeMap<K, V>::get(K& key)
+//{
+//	auto it = keyValueMap.find(key);
+//	if (it != keyValueMap.end())
+//	{
+//		return it->second;
+//	}
+//	else
+//	{
+//		return V();
+//	}
+//}
+//
+//template<typename K, typename V>
+//inline BinaryTree<K> TreeMap<K, V>::keySet()
+//{
+//	set<K> treeSet;
+//	
+//}
+//
+//
+//
+//template <class K, class V>
+//void TreeMap<K, V>::put(K& key, V& value)
+//{
+//	if (root == nullptr)
+//	{
+//		root = new BSTNode<K>(key);
+//		keyValueMap[key] = value;
+//		numElements++;
+//		
+//	}
+//	else
+//	{
+//		root->add(key);
+//		keyValueMap[key] = value;
+//		numElements++;
+//	}
+//	
+//}
+//
+//
+//
+//template <class K, class V>
+//int TreeMap<K, V>::size()
+//{
+//	return numElements;
+//}
+//
+//
+//
+//
+//
--- SearchTreeTest/TestMap.cpp
+++ SearchTreeTest/TestMap.cpp
@@ -1,136 +1,20 @@
-#pragma once
+#include <iostream>
+
 #include "CppUnitTest.h"
-#include "../CA2-SearchTree/BinaryTree.h"
-#include "../CA2-SearchTree/TreeMap.h"
+#include "../CA2-SearchTree/Entity.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace std;
 
-namespace SearchTreeTest
+namespace TestMap
 {
-	TEST_CLASS(TestMap)
+	TEST_CLASS(TestEntity)
 	{
 	public:
-		//Testss for adding elements into the treeMap
-		TEST_METHOD(TestAddingOneElementIntoTree)
+		TEST_METHOD(TestConstructor)
 		{
-			TreeMap<int, string> tree;
-			int key1 = 5;
-			string word1 = "Hello";
-			tree.put(key1, word1);
-
-			Assert::IsNotNull(tree.root);
+			Entity<int, int> tree(1, 1);
+			Assert::IsTrue(1, )
 		}
-
-		TEST_METHOD(TestAddingMultipleElementIntoTree)
-		{
-			TreeMap<int, string> tree;
-			int key1 = 5, key2 = 10, key3 = 7;
-			string word1 = "Hello", word2 = "Bye", word3 ="Hey";
-			tree.put(key1, word1);
-			tree.put(key2, word2);
-			tree.put(key3, word3);
-
-			Assert::AreEqual(3, tree.size());
-		}
-
-		//Testing clear method in TreeMap
-		TEST_METHOD(TestRemovedAllElements)
-		{
-			TreeMap<int, string> tree;
-			int key1 = 5, key2 = 10, key3 = 7;
-			string word1 = "Hello", word2 = "Bye", word3 = "Hey";
-			tree.put(key1, word1);
-			tree.put(key2, word2);
-			tree.put(key3, word3);
-
-			Assert::AreEqual(3,tree.size());
-			tree.clear();
-			Assert::IsNull(tree.root);
-		}
-
-		TEST_METHOD(TestRemovedAllWithSizeCheck)
-		{
-			TreeMap<int, string> tree;
-			int key1 = 5, key2 = 10, key3 = 7;
-			string word1 = "Hello", word2 = "Bye", word3 = "Hey";
-			tree.put(key1, word1);
-			tree.put(key2, word2);
-			tree.put(key3, word3);
-
-			Assert::AreEqual(3, tree.size());
-			tree.clear();
-			Assert::AreEqual(0, tree.size());
-			Assert::IsNull(tree.root);
-		}
-
-		//Testing for contains key method in TreeMap
-		TEST_METHOD(TestContainsKey)
-		{
-			TreeMap<int, string> tree;
-			int key1 = 5, key2 = 10, key3 = 7;
-			string word1 = "Hello", word2 = "Bye", word3 = "Hey";
-			tree.put(key1, word1);
-			tree.put(key2, word2);
-			tree.put(key3, word3);
-
-			Assert::IsTrue(tree.containsKey(key3));
-		}
-        
-		TEST_METHOD(TestNoContainsKey)
-		{
-			TreeMap<int, string> tree;
-			int key1 = 5, key2 = 10, key3 = 7, key4 = 8;
-			string word1 = "Hello", word2 = "Bye", word3 = "Hey", word4 = "See Ya";
-			tree.put(key1, word1);
-			tree.put(key2, word2);
-			tree.put(key3, word3);
-
-			Assert::IsFalse(tree.containsKey(key4));
-		}
-
-		TEST_METHOD(TestContainsKeyEmptyTreeMap)
-		{
-			TreeMap<int, int> tree;
-			int key = 5;
-			Assert::IsFalse(tree.containsKey(key));
-		}
-
-		//Testsing get method from treeMap
-		TEST_METHOD(TestGettingWord)
-		{
-			TreeMap<int, string> tree;
-			int key1 = 5;
-			string word1 = "Hello";
-			tree.put(key1, word1);
-
-			Assert::AreEqual(word1, tree.get(key1));
-		}
-
-		TEST_METHOD(TestNoGettingWord)
-		{
-			TreeMap<int, string> tree;
-			int key1 = 5, key2 = 10, key3 = 7, key4 = 2;
-			string word1 = "Hello", word2 = "Bye", word3 = "Hey", word4 = "";
-			tree.put(key1, word1);
-			tree.put(key2, word2);
-			tree.put(key3, word3);
-			tree.put(key4, word4);
-
-			Assert::AreEqual(word4, tree.get(key4));
-		}
-
-		TEST_METHOD(TestGettingRightWord)
-		{
-			TreeMap<int, string> tree;
-			int key1 = 5, key2 = 10, key3 = 7;
-			string word1 = "Hello", word2 = "Bye", word3 = "Hey";
-			tree.put(key1, word1);
-			tree.put(key2, word2);
-			tree.put(key3, word3);
-
-			Assert::AreEqual(word2, tree.get(key2));
-		}
-
-
 	};
 }
